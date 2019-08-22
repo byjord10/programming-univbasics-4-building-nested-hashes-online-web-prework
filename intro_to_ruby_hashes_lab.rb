ff56089fd6f65830eb9d19fe22783764a771e810
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-22 14:32:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,6 @@ end
   # keyword "return." Nice and neat.
 
 def monopoly_with_second_tier
-	
-	}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
